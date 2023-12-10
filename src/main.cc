99f944e8ff7667b089a1c077066b4d05d840256e
Colin Schultz <colin.r.schultz@gmail.com>
2023-12-10 13:28:25 -0800

    use args

--- src/main.cc
+++ src/main.cc
@@ -2,11 +2,15 @@
 #include "parse.cc"
 #include <iostream>
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "Missing filename argument" << std::endl;
+        return 1;
+    }
     std::vector<clause> clauses;
-    int num_vars = parse("../small_files/pigeon5.txt", clauses);
+    int num_vars = parse(argv[1], clauses);
     print_clauses(clauses);
     AdjListSolver solver(std::move(clauses), num_vars);
-    // std::cout << solver.solve() << std::endl;
+    std::cout << solver.solve() << std::endl;
     return 0;
 }
